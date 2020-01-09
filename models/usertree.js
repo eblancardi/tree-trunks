79de42c4a1d1f20cb3f027520fc671f2dc2022db
Sadigova Farida <55064917+ironhacker-sadigova@users.noreply.github.com>
2020-01-09 21:16:04 +0100

    data tree

--- models/usertree.js
+++ models/usertree.js
@@ -3,16 +3,16 @@
  
   const UserTreeSchema = new mongoose.Schema({
     name: String,
-    creator: String,
-    family: String, enum: [], // to define afterwards 
+    family: String, // enum: [], // to define afterwards 
     shape: String,
     origin: String,
     description: String,
-    availability: String, enum: [], // to define afterwards
+    availability: Number, // enum: [], // to define afterwards
     environment: String,
     climate: String,
     author: String,
     bloom: Array,
+
     
     createdAt: {
       type: Date,
@@ -23,12 +23,3 @@
   const UserTree = mongoose.model('Post', UserTreeSchema)
    
   module.exports = UserTree
-  /*
-- Creator (reference to user name)
-- Type (enum, vers catalogue)
-- Climate (type string)
-- Bloom (type enum)
-- Availability (country) (type enum)
-- Description (type string)
-FARIDA LA REPUTEE DES BETISES 
-	*/
