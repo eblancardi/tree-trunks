e1b5ec206f46ab1114e366ce49c50aa8e1634844
Enelyah <ms.jen.christine@gmail.com>
2020-01-09 20:54:54 +0100

    basic pages - all trees, user, user trees

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,11 @@ const express = require('express');
 const user_router  = express.Router();
 
 user_router.get('/:id', (req, res, next) => {
-  res.render('profile/myprofile');
+  res.render('user/user');
 });
 
 user_router.get('/:id/trees', (req, res, next) => {
-  res.render('profile/mytrees');
+  res.render('user/usertrees');
 });
 
 module.exports = user_router;
