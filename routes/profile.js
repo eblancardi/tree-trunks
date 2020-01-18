2452eb95a105329dfec25f6b47d967f0808b34b9
Enelyah <ms.jen.christine@gmail.com>
2020-01-18 17:18:36 +0100

    edit profile ok

--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,7 @@ const profile_router  = express.Router();
 const userTree = require('../models/usertree')
 const ensureLogin = require("connect-ensure-login");
 const User = require ("../models/user");
+const uploadCloud = require('../config/cloudinary.js');
 
 //profile_router.get('/', ensureLogin.ensureLoggedIn(), (req, res, next) => {
 //  res.render('profile/myprofile', {user: req.user });
@@ -28,22 +29,40 @@ profile_router.get('/mytrees', ensureLogin.ensureLoggedIn(), (req, res, next) =>
 
 // route edit profile
 profile_router.get('/edit', (req, res, next) => {
-  res.render('profile/editprofile')
+  res.render('profile/editprofile',{ user: req.user })
 });
 
-profile_router.post('/edit', (req, res) => {
-  //
+profile_router.post('/edit', uploadCloud.single('photo'), (req, res,next) => {
   if (!req.user) {
     res.redirect('/login');
     return;
   }
 
-  // à débugger pour mettre à jour les users avec edituser
-console.log(req.user);
-  req.user.email = req.body.email;
-  req.user.save().then(user => {
-    res.render('profile/myprofile', {user: req.user}) // exposer user lien avec db 
-  }).catch(next);
+if (req.file){
+  req.user.avatar = req.file.url;
+  User.findByIdAndUpdate(req.user._id, { $set: {avatar: req.user.avatar, email:req.user.email} }, {new:true})
+//req.user.save()
+    .then(res.redirect(req.originalUrl))
+   // return
+  .catch(next);
+};
+if (req.body.email!==req.user.email){
+  req.user.email=req.body.email;
+  User.findByIdAndUpdate(req.user._id, { $set: {email: req.user.email} }, {new:true})
+//req.user.save()
+  .then(res.redirect(req.originalUrl)
+  ).catch(next);
+};
+
 })
+
+profile_router.post('/delete', (req, res, next) => {
+  User.findByIdAndRemove(req.user.id)
+    .then(usere => res.redirect('/'))
+    .catch(err => next(err));
+});
+// User.deleteOne({ name: "Alice"})
+//   .then(successCallback)
+//   .catch(errorCallback);
 
 module.exports = profile_router;
