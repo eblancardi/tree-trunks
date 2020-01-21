e8766008c1ddae2ca89321c6de09dde0c78b8cd9
Enelyah <ms.jen.christine@gmail.com>
2020-01-21 21:55:31 +0100

    user page start

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const user_router  = express.Router();
-const uploadCloud = require('../config/cloudinary.js'); 
+const User = require ("../models/user");
 
 user_router.get('/:id', (req, res, next) => {
-  res.render('user/user');
+  User.findOne({ username: req.params.id })
+  .populate(trees)
+  
+  .then( user => {
+    console.log(user)
+    res.render('user/user', {user:user} )
+  })
+  .catch(next)
 });
 
-user_router.get('/:id/trees', (req, res, next) => {profile
-  res.render('user/usertrees', );
+user_router.get('/:id/trees', (req, res, next) => {
+  User.findOne({ username: req.params.id })
+  .populate(trees)
+  .then( user =>
+    res.render('user/usertrees', {user:user} )
+  )
+  .catch(next)
 });
-
 
 
 module.exports = user_router;
